eb40b7e23650433784c52a0218f4a8c9a4822850
Sumitha Viswanathan <104032529+vt-sviswanathan@users.noreply.github.com>
2022-06-03 06:33:21 -0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
     return(
-        <div className="navbar">
+        <div className="navbar test11111">
             <h1>
                 Let&apos;s find the definitions!
             </h1>
